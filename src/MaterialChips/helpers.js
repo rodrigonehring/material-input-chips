3a3c15afd92d19dcce7d68157ad00e3247f71ce0
rodrigo <rodrigonehring@hotmail.com>
2017-10-18 13:31:05 +0000

    added eslint and fixed some rules

--- src/MaterialChips/helpers.js
+++ src/MaterialChips/helpers.js
@@ -1,5 +1,5 @@
-// import validateEmail from 'helpers/checkEmail';
-const validateEmail = () => true;
+// import validateEmail from 'helpers/checkEmail'
+const validateEmail = () => true
 
 export const TYPES = {
   ESCAPE: [27],
@@ -7,26 +7,26 @@ export const TYPES = {
   RIGHT: [39],
   BACKSPACE: [8],
   TAB: [9],
-  DELETE_CODES: [46, 8]
-};
+  DELETE_CODES: [46, 8],
+}
 
 export function acceptedKeycodes(code) {
-  return code > 47 && code < 91;
+  return code > 47 && code < 91
 }
 
 export function validate(value, validators, selected) {
-  let error;
+  let error
 
   // with for can break loop
   for (let i = 0; i < validators.length; i++) {
-    const current = validators[i];
+    const current = validators[i]
     if (current.validator(value, selected)) {
-      error = current.message;
-      break;
+      error = current.message
+      break
     }
   }
 
-  return error;
+  return error
 }
 
 // por ordem de prioridade
@@ -34,7 +34,7 @@ export const defaultValidators = [
   // { message: 'Precisa ter length maior que 3', validator: (value) => value && value.length < 2 },
   { message: 'Verifique o(s) e-mail(s) informado(s)', validator: value => !validateEmail(value) },
   { message: 'Já existe este email na lista', validator: (value, selected) => {
-    const items = selected.map(item => item.Email);
-    return items.includes(value);
-  } }
-];
+    const items = selected.map(item => item.Email)
+    return items.includes(value)
+  } },
+]

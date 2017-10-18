f763c93370c9f026838980e6e381ee2df1386adb
rodrigo <rodrigonehring@hotmail.com>
2017-10-18 17:02:41 +0000

    deploy

--- deploy.js
+++ deploy.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 
-
 const getCurrentTag = () => new Promise((resolve, reject) => {
   exec('git describe --abbrev=0 --tags', (err, stdout) => {
     if (err) {
@@ -13,6 +12,17 @@ const getCurrentTag = () => new Promise((resolve, reject) => {
     resolve(stdout.replace('\n', ''));
   });
 });
+
+const getCurrentBranch = () => new Promise((resolve, reject) => {
+  exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+    if (err) {
+      return reject(err);
+    }
+
+    resolve(stdout.replace('\n', ''));
+  });
+});
+
 
 const openPackage = () => new Promise((resolve, reject) => {
   fs.readFile('./package.json', 'utf8', (err, data) => {
@@ -36,7 +46,7 @@ const savePackage = (content) => new Promise((resolve, reject) => {
 
 
 const commit = (version) => new Promise((resolve, reject) => {
-  exec(`git add . && git commit -m "Auto updating version" && git tag ${version} -f && git push && git push --tags`, (err, stdout) => {
+  exec(`git add . && git commit -m "Auto updating version" && git tag ${version} -f && git push && git push --tags -f`, (err, stdout) => {
     if (err) {
       return reject(err);
     }
@@ -48,10 +58,12 @@ const commit = (version) => new Promise((resolve, reject) => {
 
 async function deploy() {
   const currentTag = await getCurrentTag();
+  const currentBranch = await getCurrentBranch();
   const packageJson = await openPackage();
 
-  console.log('currrent tag version: ', currentTag);
-  console.log('currrent package.json version: ', packageJson.version);
+  console.log('current branch: ', currentBranch);
+  console.log('current tag version: ', currentTag);
+  console.log('current package.json version: ', packageJson.version);
 
   if (currentTag !== packageJson.version) {
     console.log('Different versions, updating package.json');

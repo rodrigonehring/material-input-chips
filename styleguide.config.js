c925c397b9fe4ecbb5ddfbab6299d153c611557f
nehring <rodrigonehring@hotmail.com>
2017-10-24 19:45:02 +0000

    styleguidist build fix

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,19 @@
 const path = require('path')
 
 module.exports = {
-  webpackConfig: require('./webpack.config.js'),
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+  },
   require: [
     path.resolve(__dirname, 'styleguide/setup.js'),
   ],
